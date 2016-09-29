1d149f0bacd85670c46e51fdc51070753081ce4b
Peter Ziska <peter@ziska.sk>
2016-09-29 13:13:33 +0000

    pridana hlavicka

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+/********************************************************************************
+   *    Predmet:        Formalne jazyky a prekladace (IFJ), Algoritmy (IAL)     *
+   *    Projekt:        Interpret zjednoduseneho jazyka Java SE                 *
+   *    Variant:        b/2/II                                                  *
+   *    Tim:            092                                                     *
+   *    Subor:          main.c                                               *
+   *    Riesitelia:     Filip Mik       (xmikfi00@stud.fit.vutbr.cz)            *
+   *                    Peter Ziska     (xziska02@stud.fit.vutbr.cz)            *
+   *                    Peter Pristas   (xprist05@stud.fit.vutbr.cz)            *
+   *                    Daniel Florek   (xflore02@stud.fit.vutbr.cz)            *
+   *                    Martin Grnac    (xgrnac00@stud.fit.vutbr.cz)            *
+   *                                                                            *               
+   ********************************************************************************/
+
 #include "scaner.h"
 #include <stdbool.h>
 
