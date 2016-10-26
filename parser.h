3783a6d88d6f672643cf7e30c762c11c6656d327
Filip Mik <ifjproj2016@gmail.com>
2016-10-26 21:49:00 +0200

    enum do stack

--- parser.h
+++ parser.h
@@ -15,3 +15,36 @@
    int parser();
    int prog();
    int after_class();
+
+   typedef enum{  
+      T_EOF,        // 0 
+      T_smcln,      // 1 semicolon ; 
+      N_P,          // 2 Program- vstupny N
+      N_C,          // 3 Class
+      T_class,      // 4 klucove slovo class
+      T_idc,        // 5 identifikator classy
+      T_L_KOSZ,     // 6 prava kosata zatvorka
+      T_P_KOSZ,     // 7 lava kosata zatvorka
+      N_AC,         // 8 after class
+      N_F,          // 9 funckia
+      N_DEFS,       // 10 staticka premenna 
+      T_static,     // 11 klucove slovo static
+      T_type,       // 12 klucove slovo type
+      T_id,         // 13 identifikator premennej
+      N_I,          // 14 inicializacia
+      T_rov,        // 15 rovnatko =
+      N_E,          // 16 expression
+      N_DEFC,       // 17 definicia lokalnej premennej  
+      T_idf,        // 18 identifikator funkcie
+      N_AL,         // 19 argument list
+      N_AD,         // 20 argument definition
+      N_ALE,        // 21 argument list extension
+      N_S,          // 22 statement
+      T_LZAT,       // 23 lava zatvorka
+      T_PZAT,       // 24 prava zatvorka
+      T_if,         // 25 if
+      T_else,       // 26 else
+      T_while,      // 27 while
+      T_return,     // 28 return
+      /*--------to be continued---------*/
+}TGrammar;
