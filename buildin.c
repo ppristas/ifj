073262515e1902cc7188714932937173e9ed69db
Martin Grnáč <spekov@gmail.com>
2016-10-27 11:45:23 +0000

    sort oprava

--- buildin.c
+++ buildin.c
@@ -4,7 +4,8 @@
 // v interprete uvolnovat vystup str_ret 
 char* sort(const char *str)
 {
-    int n = strlen(str);
+    unsigned int n = strlen(str);
+
     char *str_tmp=NULL;
     char *str_ret=NULL;
     if ((str_ret=malloc(sizeof(char)*(n+1))) == NULL)
@@ -15,11 +16,11 @@ char* sort(const char *str)
 
     strncpy(&str_tmp[1],str,n);
     str_tmp[n+1]='\0';
-    str_tmp[0]=(n);
+    str_tmp[0]='#';
 
     memset(str_ret,0,n+1);
 
-    heapsort(str_tmp,str_ret);
+    heapsort(str_tmp,str_ret,n);
 
     free(str_tmp);
     return str_ret;
