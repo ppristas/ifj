4d536ddd648f5225ffa1eb439a53e4c56b754dea
Martin Grnáč <spekov@gmail.com>
2016-10-27 09:59:20 +0000

    buildin.c created sort

---
+++ buildin.c
@@ -0,0 +1,26 @@
+#include "ial.h"
+
+
+// v interprete uvolnovat vystup str_ret 
+char* sort(const char *str)
+{
+    int n = strlen(str);
+    char *str_tmp=NULL;
+    char *str_ret=NULL;
+    if ((str_ret=malloc(sizeof(char)*(n+1))) == NULL)
+        return NULL;
+
+    if ((str_tmp=malloc(sizeof(char)*(n+2))) == NULL )
+        return NULL;
+
+    strncpy(&str_tmp[1],str,n);
+    str_tmp[n+1]='\0';
+    str_tmp[0]=(n);
+
+    memset(str_ret,0,n+1);
+
+    heapsort(str_tmp,str_ret);
+
+    free(str_tmp);
+    return str_ret;
+}
