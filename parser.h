a598da356710e22dde35bec6885280f157ae3521
Filip Mik <ifjproj2016@gmail.com>
2016-11-01 20:34:00 +0100

    enum pre vstavane funkcie

--- parser.h
+++ parser.h
@@ -12,6 +12,7 @@
    *                                                                            *               
    ********************************************************************************/
 
+   int is_build_function();
    int parser();
    int prog();
    int class();
@@ -20,6 +21,19 @@
    int main_body();
    int build_print();
    int print_params();
-   int build_function_call();
+   int build_function_call(int decider);
 
    /*void expand(tStack *p_stack, int num,...); */
+
+   typedef enum{
+   F_NbIF,        // 0 not build in function
+   F_print,       // 1 print
+   F_int,         // 2 readint
+   F_double,      // 3 readDouble
+   F_string,      // 4 readString
+   F_length,      // 5 length
+   F_substr,      // 6 substr
+   F_compare,     // 7 compare
+   F_find,        // 8 compare
+   F_sort,        // 9 sort
+}BI_Funkce;
