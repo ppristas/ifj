aaa018678ff96eadc85c655e2fd0e5b027178203
xziska02 <peter@ziska.sk>
2016-11-10 21:51:32 +0100

    jebnuty git *nic som nemenil aj tak to hadze error

--- ial.c
+++ ial.c
@@ -271,7 +271,7 @@ char* heapsort(char *ptr,unsigned int n)
         stifDown(ptr,i,right);
     }
 
-    unsigned int i=0;
+    //unsigned int i=0;
 
     for(right = n;right > 1; --right)
     {
@@ -353,3 +353,4 @@ int boyer_moore(const char *str, const char *pattern){
     return -1;
 
 }
+
