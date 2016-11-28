373fe75c8030cb15eb5b5524f53151d5a1b44316
Daniel Florek <Tranesvk@gmail.com>
2016-11-28 01:50:33 +0000

    Upravená inštrukčná páska tak, aby bolo možné vytvárať lokálne pásky pre jednotlivé funkcie a aby bolo možné generovať skoky.

--- ilist.c
+++ ilist.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include "ilist.h"
 
-ilist instrList;
 
-void generateInstruction(eInstrType type, void *op1, void *op2, void *dest)
+//Globálna páska na inštrukcie týkajúce sa inštrukcií globálnych premenných
+ilist globalList;
+ilist mainList;
+
+//Generovanie inštrukcií na koniec danej inštrukčnej pásky
+void generateLastInstruction(eInstrType type, void *op1, void *op2, void *dest, ilist *L)
 {
     instr *tmp;
 
@@ -19,9 +23,28 @@ void generateInstruction(eInstrType type, void *op1, void *op2, void *dest)
     tmp->dest=dest;
     tmp->instrType=type;
 
-    insertInstruction(&instrList, tmp);
+    insertLastInstruction(L, tmp);
 }
 
+//Generovanie inštrukcií za aktívny prvok
+void generatePostInstruction(eInstrType type, void *op1, void *op2, void *dest, ilist *L){
+    instr *tmp;
+
+    tmp = malloc(sizeof(instr));
+
+    if(tmp == NULL){
+        return;
+    }
+
+    tmp->op1=op1;
+    tmp->op2=op2;
+    tmp->dest=dest;
+    tmp->instrType=type;
+
+    insertPostIntruction(L, tmp);
+}
+
+//Inicializacia Listu
 void listInit(ilist *L)
 {
     L->active=NULL;
@@ -29,19 +52,37 @@ void listInit(ilist *L)
     L->last=NULL;
 }
 
-void first(ilist *L)
+//Nastavenie aktívneho prvku na prvý prvok
+void actFirst(ilist *L)
 {
     if(L->first != NULL){
         L->active = L->first;
     }
 }
 
+//Nastavenie aktívneho prvku na posledný prvok
+void actLast(ilist *L){
+    if(L->last != NULL){
+        L->active = L->last;
+    }
+}
+
+//Nastavenie aktívneho prvku na predchádzajúci prvok
+void prevInstruction(ilist *L){
+    if(L->active != NULL){
+        L->active = L->active->prevItem;
+    }
+}
+
+
+//Iterácia aktívneho prvku o jedno dopredu
 void succ(ilist *L)
 {
     L->active = L->active->nextItem;
 }
 
-void insertInstruction(ilist *L, instr *I)
+//Toto nie je nič pre vás
+void insertLastInstruction(ilist *L, instr *I)
 {
     item* tmp;
 
@@ -50,7 +91,7 @@ void insertInstruction(ilist *L, instr *I)
         return;
     }
 
-    if(L->first==NULL){
+    if(L->first==NULL && L->last==NULL){
         tmp->instrPtr = I;
         tmp->prevItem = NULL;
         tmp->nextItem = NULL;
@@ -66,6 +107,40 @@ void insertInstruction(ilist *L, instr *I)
     }
 }
 
+//Toto nie je nič pre vás
+void insertPostIntruction(ilist *L, instr *I){
+    if(L->active == NULL){
+        return;
+    }
+
+    item* tmp;
+
+    tmp = malloc(sizeof(item));
+    if(tmp == NULL){
+        return;
+    }
+
+    if(L->active->nextItem != NULL){
+        tmp->instrPtr = I;
+        tmp->prevItem = L->active;
+        tmp->nextItem = L->active->nextItem;
+        tmp->nextItem->prevItem=tmp;
+        L->active->nextItem = tmp;
+
+    }
+
+    if(L->active->nextItem == NULL){
+        tmp->prevItem = L->active;
+        tmp->nextItem = NULL;
+        L->active->nextItem = tmp;
+        tmp->instrPtr=I;
+    }
+
+    L->active = tmp;
+
+}
+
+//Uvoľní sa daná páska
 void destroyList(ilist *L)
 {
     item* tmp;
@@ -78,16 +153,19 @@ void destroyList(ilist *L)
     }
 }
 
+//Nič pre vás, získanie inštrukcie z aktívneho prvku
 instr *getInstruction(ilist *L)
 {
     return L->active->instrPtr;
 
 }
 
+//Získa sa posledný prvok Inštrukčnej pásky
 item *getLast(ilist *L){
     return L->last;
 }
 
-void setInstruction(ilist *L, item * instruction){
-    L->active=instruction;
+//Inštrukcia na skoky
+void setInstruction(ilist *L, item *instruction){
+    L->active = instruction;
 }
