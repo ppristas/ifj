129589d2eb5ae916e31b179a3d1c076be8beb286
Martin Grnáč <spekov@gmail.com>
2016-11-30 21:28:55 +0000

    pridane offsety

--- temp_tab.h
+++ temp_tab.h
@@ -18,15 +18,12 @@ typedef struct tmp_item
 
 typedef T_TMP_ITEM* TMP_HTAB[TMP_HTAB_SIZE];
 
-void tmp_htab_insert(TMP_HTAB* table,symbolType type,void* ptr,char* key);
+void tmp_htab_insert(TMP_HTAB* table,symbolType type,void* ptr,char* key,int offset);
 T_TMP_ITEM* tmp_htab_search(TMP_HTAB* table,char* key);
 symData* tmp_get_data(TMP_HTAB* table,char* key);
 void tmp_htab_init(TMP_HTAB* table);
 
-void Print_table(TMP_HTAB* tabulka);
-void Print_table_row(T_TMP_ITEM* item);
 
-extern TMP_HTAB* middle_vysledky;
-extern TMP_HTAB* const_table;
+extern TMP_HTAB* tabulka;
 
 #endif
