e877c1fbdd2829eecab7751b1e1bf21ebd734878
Daniel Florek <Tranesvk@gmail.com>
2016-12-07 22:36:35 +0000

    Upravene Builtiny

--- buildin.h
+++ buildin.h
@@ -6,13 +6,11 @@
 //TEST SMART-GIT 
 
 char* sort(const char *str);
-int length(const char* s);
 char* substring(const char *s, const int i, const int n);
-int compare(const char *s1, const char *s2);
 int find(char* s,char* search);
 
-void readInt(symData * dest);
+int readInt(symData * dest);
 void readString(symData * dest);
-void readDouble(symData * dest);
+int readDouble(symData * dest);
 
 #endif
